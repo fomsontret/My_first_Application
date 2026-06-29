455bfa45dca9dfd652e341ba597eb2a85b8aef39
DESKTOP-6LSG4AU\German <fomsontret@yandex.ru>
2026-06-29 15:31:12 +0300

    sqlLite

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -2,126 +2,32 @@ package ru.netology.nmedia.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.dao.PostDao
 import ru.netology.nmedia.dto.Post
 
-class PostRepositoryImpl : PostRepository {
+class PostRepositoryImpl(private val dao: PostDao) : PostRepository {
 
-    private var posts = listOf(
-
-        Post (
-            id = 7,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология!...",
-            likes = 1008,
-            likeByMe = false,
-            reposts = 5399
-        ),
-
-        Post (
-            id = 6,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология!...",
-            likes = 170,
-            likeByMe = false,
-            reposts = 5399
-        ),
-
-        Post (
-            id = 5,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология!...",
-            likes = 600,
-            likeByMe = false,
-            reposts = 5399
-        ),
-
-        Post (
-            id = 4,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...Привет, это новая Нетология!...",
-            likes = 500,
-            likeByMe = false,
-            reposts = 5399
-        ),
-
-        Post (
-            id = 3,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология!...",
-            likes = 30,
-            likeByMe = false,
-            reposts = 5399
-        ),
-
-        Post (
-            id = 2,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "18 сентября в 10:20",
-            content = "Знаний на всех хватит, сегодня мы разбираемся с разработкой мобильных приложений",
-            likes = 10,
-            likeByMe = false,
-            reposts = 5399
-        ),
-
-        Post (
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология!...",
-            likes = 109,
-            likeByMe = false,
-            reposts = 5399,
-            video = "https://rutube.ru/video/6550a91e7e523f9503bed47e4c46d0cb"
-        )
-    )
-
-    private val  data = MutableLiveData(posts)
+    private val data = MutableLiveData(dao.getAll())
 
     override fun getAll(): LiveData<List<Post>> = data
 
-    override fun likeById(id : Long) {
-        posts = posts.map {
-            if (it.id != id) it else it.copy(likeByMe = !it.likeByMe, likes = if (it.likeByMe) it.likes - 1 else it.likes +1)
-        }
-        data.value = posts
+    override fun likeById(id: Long) {
+        dao.likeById(id)
+        data.value = dao.getAll()
     }
 
     override fun repost(id: Long) {
-        posts = posts.map {
-           if (it.id != id) it else it.copy (reposts = it.reposts + 1)
-        }
-        data.value = posts
+        dao.repost(id)
+        data.value = dao.getAll()
     }
 
     override fun removeByID(id: Long) {
-        posts = posts.filterNot { it.id == id }
-        data.value = posts
+        dao.removeById(id)
+        data.value = dao.getAll()
     }
 
     override fun save(post: Post) {
-        if (post.id == 0L) {
-            posts  = listOf(
-                post.copy (
-                author = "Me",
-                likes = 0,
-                likeByMe = false,
-                reposts = 0,
-                published = "Now" )
-            ) + posts
-        } else {
-            posts = posts.map {
-                if (it.id == post.id){
-                    it.copy (content = post.content)
-                } else {
-                    it
-                }
-            }
-        }
-        data.value = posts
+        dao.save(post)
+        data.value = dao.getAll()
     }
 }
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -3,38 +3,37 @@ package ru.netology.nmedia.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.db.AppDb
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
-import ru.netology.nmedia.repository.PostRepositoryFileImpl
+import ru.netology.nmedia.repository.PostRepositoryImpl
 
+private val emptyPost = Post()
 
-private val emptyPost = Post ()
-
-class PostViewModel(application : Application) : AndroidViewModel(application) {
-    private val repository: PostRepository = PostRepositoryFileImpl(application)
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: PostRepository = PostRepositoryImpl(
+        AppDb.getInstance(application).postDao
+    )
     val data = repository.getAll()
 
     fun likeById(id: Long) = repository.likeById(id)
     fun repost(id: Long) = repository.repost(id)
-
     fun removeById(id: Long) = repository.removeByID(id)
 
-    val edited = MutableLiveData (emptyPost)
+    val edited = MutableLiveData(emptyPost)
 
-    fun save (content: String) {
-        edited.value?. let { post ->
-            val trimmed: String = content.trim()
+    fun save(content: String) {
+        edited.value?.let { post ->
+            val trimmed = content.trim()
             if (trimmed != post.content) {
-                repository.save(post.copy(content = content))
+                repository.save(post.copy(content = trimmed))
             }
             edited.value = emptyPost
         }
-
     }
 
     fun edit(post: Post) {
         edited.value = post
-
     }
 
     fun cancelEdit() {
